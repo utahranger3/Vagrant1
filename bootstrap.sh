d324136325cbf57fd28f51907fda3b6ead34a722
utahranger3 <42624355+utahranger3@users.noreply.github.com>
2018-09-30 11:01:16 -0600

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,3 +8,17 @@
       rm -rf /var/www
       ln -fs /vagrant /var/www
     fi
+sudo mkdir /vagrant/virtualhosts
+sudo mkdir /vagrant/virtualhosts/default
+sudo echo "<VirtualHost *:80>
+  DocumentRoot /var/www/html/web
+</VirtualHost>" > /vagrant/virtualhosts/default/000-default.conf
+sudo rm /etc/apache2/sites-enabled/000-default.conf
+sudo ln -s /vagrant/virtualhosts/default/000-default.conf /etc/apache2/sites-enabled/000-default.conf
+sudo mkdir /var/www/html
+sudo mkdir /var/www/html/web
+sudo touch /var/www/html/web/index.php
+sudo echo "<?php
+phpinfo();
+?>" > /var/www/html/web/index.php
+sudo service apache2 reload
